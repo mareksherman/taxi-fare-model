8e4be90f63e5bebca57b18849355be591e4e0b93
Marek Sherman <marek.sherman@gmail.com>
2021-11-17 14:55:56 +0100

    Finished mlflow challenge

--- TaxiFareModel/trainer.py
+++ TaxiFareModel/trainer.py
@@ -6,6 +6,14 @@ from sklearn.linear_model import LinearRegression
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from sklearn.pipeline import Pipeline
+import mlflow
+from mlflow.tracking import MlflowClient
+from memoized_property import memoized_property
+import joblib
+from termcolor import colored
+
+MLFLOW_URI = "https://mlflow.lewagon.co/"
+EXPERIMENT_NAME = "[DE] [Munich] [mareksherman] lin reg v.01"
 
 class Trainer():
     def __init__(self, X, y):
@@ -16,6 +24,11 @@ class Trainer():
         self.pipeline = None
         self.X = X
         self.y = y
+        self.experiment_name = EXPERIMENT_NAME
+
+    def set_experiment_name(self, experiment_name):
+        '''defines the experiment name for MLFlow'''
+        self.experiment_name = experiment_name
 
     def set_pipeline(self):
         """defines the pipeline as a class attribute"""
@@ -45,7 +58,37 @@ class Trainer():
     def evaluate(self, X_test, y_test):
         """evaluates the pipeline on df_test and return the RMSE"""
         y_pred = self.pipeline.predict(X_test)
-        return compute_rmse(y_pred, y_test)
+        rmse = compute_rmse(y_pred, y_test)
+        self.mlflow_log_metric("rmse", rmse)
+        return round(rmse,2)
+
+    def save_model(self):
+        """Save the model into a .joblib format"""
+        joblib.dump(self.pipeline, 'model.joblib')
+        print(colored("model.joblib saved locally", "green"))
+
+    @memoized_property
+    def mlflow_client(self):
+        mlflow.set_tracking_uri(MLFLOW_URI)
+        return MlflowClient()
+
+    @memoized_property
+    def mlflow_experiment_id(self):
+        try:
+            return self.mlflow_client.create_experiment(self.experiment_name)
+        except BaseException:
+            return self.mlflow_client.get_experiment_by_name(
+                self.experiment_name).experiment_id
+
+    @memoized_property
+    def mlflow_run(self):
+        return self.mlflow_client.create_run(self.mlflow_experiment_id)
+
+    def mlflow_log_param(self, key, value):
+        self.mlflow_client.log_param(self.mlflow_run.info.run_id, key, value)
+
+    def mlflow_log_metric(self, key, value):
+        self.mlflow_client.log_metric(self.mlflow_run.info.run_id, key, value)
 
 
 if __name__ == "__main__":
@@ -60,4 +103,10 @@ if __name__ == "__main__":
     time_features = time_enc.fit_transform(X_train, y_train)
     trainer = Trainer(X_train,y_train)
     trainer.run()
-    print(trainer.evaluate(X_test,y_test))
+    rsme = trainer.evaluate(X_test,y_test)
+    print(rsme)
+    trainer.save_model()
+    experiment_id = trainer.mlflow_experiment_id
+
+    print(
+    f"experiment URL: https://mlflow.lewagon.co/#/experiments/{experiment_id}")
